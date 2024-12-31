bcad1c0a92688b0be22f51fdb8419ad5b541b288
stooone <4orestelements@gmail.com>
2024-12-31 21:14:32 +0900

    ファイル読み込み実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import System.Environment
-import System.IO
 import qualified Data.Text as T
 import qualified Data.Text.IO as TI
 import Lib
+import qualified System.Environment
+import qualified System.Exit
+import System.IO
+
+parseArgs :: IO FilePath
+parseArgs = do
+  args <- System.Environment.getArgs
+  let len = length args
+  if len == 0
+    then
+      System.Exit.die "You must specify an input file"
+    else
+      if len >= 2
+        then
+          System.Exit.die "You must specify exactly one input file"
+        else
+          return $ head args
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let fileName = head args
-    input <- TI.readFile fileName
-    TI.putStrLn input
-    
+  inFile <- parseArgs
+  putStrLn "fin"
+
+-- let _ = process_file inFile
+
+-- args <- getArgs
+-- let fileName = head args
+-- input <- TI.readFile fileName
+-- TI.putStrLn input
